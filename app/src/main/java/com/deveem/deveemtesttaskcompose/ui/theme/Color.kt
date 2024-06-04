a688adbe101b5ce6213dbcc9c5d2a357422b73ba
Aktan <86038270+jstAga@users.noreply.github.com>
2024-06-04 22:55:09 +0600

    create project structure

--- app/src/main/java/com/deveem/deveemtesttaskcompose/ui/theme/Color.kt
+++ app/src/main/java/com/deveem/deveemtesttaskcompose/ui/theme/Color.kt
@@ -2,10 +2,10 @@ package com.deveem.deveemtesttaskcompose.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val GrayLight = Color(0xFFECECEC)
+val White = Color(0xFFFFFFFF)
+val Black = Color(0xFF000000)
+val GrayAverage = Color(0xFFD5D5D5)
+val GrayMiddle = Color(0xFF999999)
+val GrayDark = Color(0xFF555555)
--- app/src/main/java/com/deveem/deveemtesttaskcompose/ui/theme/Type.kt
+++ app/src/main/java/com/deveem/deveemtesttaskcompose/ui/theme/Type.kt
@@ -1,34 +1,70 @@
 package com.deveem.deveemtesttaskcompose.ui.theme
 
 import androidx.compose.material3.Typography
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
+import com.deveem.deveemtesttaskcompose.R
 
-// Set of Material typography styles to start with
+private val robotoBold = FontFamily(Font(R.font.roboto_bold, FontWeight.Bold))
+private val robotoRegular = FontFamily(Font(R.font.roboto_regular, FontWeight.Normal))
+
 val Typography = Typography(
+  
+  bodySmall = TextStyle(
+    fontSize = 12.sp,
+    fontFamily = robotoRegular,
+    color = Color.Black
+  ),
+  
   bodyLarge = TextStyle(
-    fontFamily = FontFamily.Default,
-    fontWeight = FontWeight.Normal,
+    fontSize = 14.sp,
+    fontFamily = robotoRegular,
+    color = Color.Black
+  ),
+  
+  labelLarge = TextStyle(
     fontSize = 16.sp,
-    lineHeight = 24.sp,
-    letterSpacing = 0.5.sp
+    fontFamily = robotoRegular,
+    color = Color.Black
+  ),
+  
+  titleSmall = TextStyle(
+    fontSize = 18.sp,
+    fontFamily = robotoRegular,
+    color = Color.Black
+  ),
+  
+  titleMedium = TextStyle(
+    fontSize = 20.sp,
+    fontFamily = robotoBold,
+    color = Color.Black
+  ),
+  
+  headlineSmall = TextStyle(
+    fontSize = 24.sp,
+    fontFamily = robotoRegular,
+    color = Color.Black
+  ),
+  
+  headlineLarge = TextStyle(
+    fontSize = 32.sp,
+    fontFamily = robotoRegular,
+    color = Color.Black
+  ),
+  
+  displaySmall = TextStyle(
+    fontSize = 28.sp,
+    fontFamily = robotoRegular,
+    color = Color.Black
+  ),
+  
+  displayLarge = TextStyle(
+    fontSize = 48.sp,
+    fontFamily = robotoBold,
+    color = Color.Black
   )
-  /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
-    ),
-    labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
-    )
-    */
 )
